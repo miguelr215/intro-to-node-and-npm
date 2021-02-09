fcd4257f3efba3f3a8fdd297dd24753f5e2dae83
drexelramos <drexelramos@gmail.com>
2021-02-08 18:57:08 -0800

    read fs

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
+// PART 1
 // 1 + 1;
 
 // console.log("Hello world");
 
 // console.log(process.argv);
 
-let actualHours = 45;
-let hourlyRate = 50;
-let overtime = 1.5;
-let weeklyHours = 40;
 
-let overtimeHours = actualHours > weeklyHours ? actualHours - weeklyHours : 0;
-let normalHours = actualHours > weeklyHours ? weeklyHours : actualHours;
-let normalPay = normalHours * hourlyRate;
-let overtimePay = overtimeHours * hourlyRate * overtime;
+// PART 2
+// let actualHours = 45;
+// let hourlyRate = 50;
+// let overtime = 1.5;
+// let weeklyHours = 40;
 
-let totalPay = normalPay + overtimePay;
+// let overtimeHours = actualHours > weeklyHours ? actualHours - weeklyHours : 0;
+// let normalHours = actualHours > weeklyHours ? weeklyHours : actualHours;
+// let normalPay = normalHours * hourlyRate;
+// let overtimePay = overtimeHours * hourlyRate * overtime;
 
-console.log(`The total pay is $${totalPay}`);
+// let totalPay = normalPay + overtimePay;
+
+// console.log(`The total pay is $${totalPay}`);
+
+
+// PART 3
+const fs = require("fs");
+
+const contents = fs.readdirSync("./animals");
+
+console.log(contents);
 
