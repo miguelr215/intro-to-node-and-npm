0267bdf9a8274ecaafa0652823f98a7fef018f15
drexelramos <drexelramos@gmail.com>
2021-02-10 15:18:22 -0800

    updated animate-string

--- animate-string.js
+++ animate-string.js
@@ -1,8 +1,19 @@
 const chalk = require("chalk-animation");
 
-function animateString(string){
-    const animation = chalk.rainbow(string);
+// list valide methods
+const validMethods = ['glitch', 'neon', 'pulse', 'karaoke', 'rainbow', 'radar'];
 
+// grab 3rd position in args array
+const scriptArg = process.argv[2];
+
+// check the arg is valid and use it or default to 'rainbow'
+const method = validMethods.includes(scriptArg) ? scriptArg : 'rainbow';
+
+function animateString(string){
+    // const animation = chalk.rainbow(string);
+    // use the method that was selected
+    const animation = chalk[method](string);
+    
     setTimeout(() => animation.stop(), 1300);
 };
 
